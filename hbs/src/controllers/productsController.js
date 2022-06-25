3f26a9c99eeb575fb8b3596e16e9db35175c35e8
doskapps <50601180+doskapps@users.noreply.github.com>
2022-06-25 02:42:48 -0300

    Mejora en comparar si un campo del producto está vacío

--- hbs/src/controllers/productsController.js
+++ hbs/src/controllers/productsController.js
@@ -9,13 +9,13 @@ const getProducts = (req, res) => {
 
 const getProductsView = (req, res) => {
     res.render('products.hbs', { products })
-}
+ }
 
 const postProducts = (req, res) => {
     const {nombre, precio, url} = req.body
     const id = products.length ? ((products[products.length - 1].id) + 1) : products.length + 1;
     
-    if((nombre !== '' && precio !== '' && url !== '')){
+    if([nombre, precio, url].includes !== ''){
         products.push({nombre, precio, url, id})
     }
     res.status(201).redirect('/')
